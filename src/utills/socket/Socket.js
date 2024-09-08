31b970a2e5cd78c5f4c07f15a6afc68eca6bc543
sanju <sanjubora84@gmail.com>
2024-09-08 13:56:28 +0530

    update

--- src/utills/socket/Socket.js
+++ src/utills/socket/Socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { getLocalStorage } from "@/utills/LocalStorageUtills";
 
 
-const socket = io(`${import.meta.env.VITE_APP_VITEAPPLOCAL}/`, {
+const socket = io(`http://localhost:3000`, {
 // const socket = io(`http://localhost:3001/`, {
   reconnectionAttempts: 5, // Number of reconnection attempts before giving up
   transports: ['websocket'], // Use WebSocket transport
